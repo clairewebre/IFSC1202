39f7b2b66349a38789f708d35fcf9cffcd3455ca
clairewebre <142812288+clairewebre@users.noreply.github.com>
2023-09-10 15:51:03 +0000

    series

--- 04.09 Series.py
+++ 04.09 Series.py
@@ -1,1 +1,8 @@
-A = int(input)
+A = int(input("Enter A: "))
+B = int(input("Enter B: "))
+if A<B:
+    for i in range (A,B+1):
+        print(i, sep='\n')
+elif A>=B:
+    for i in range (A, B-1, -1):
+        print(i, sep='\n')

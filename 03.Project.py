822755b1d34bd6af30b67af14618725909192f03
clairewebre <142812288+clairewebre@users.noreply.github.com>
2023-09-10 16:54:35 +0000

    project 3

--- 03.Project.py
+++ 03.Project.py
@@ -1,6 +1,6 @@
-x = int(input("Enter first number: "))
+x = float(input("Enter first number: "))
 o = str(input("Enter operator (+,-,*,/): "))
-y = int(input("Enter second number: "))
+y = float(input("Enter second number: "))
 if o == "+":
     print(str(x) + " + " + str(y) + " = " + (str(x + y)))
 elif o == "-":
---
+++ 04.Project.py
@@ -0,0 +1,8 @@
+S = int(input("Enter Start of Range: "))
+E = int(input("Enter End of Range: "))
+for i in range (S,E+1):
+    if (float(i//2) == float(i%2)):
+        print(i)
+    
+
+        

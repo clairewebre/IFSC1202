c28154ea426236325b1162159e7329ce07e92251
clairewebre <142812288+clairewebre@users.noreply.github.com>
2023-09-11 14:51:24 +0000

    is prime

--- 04.08 Is Prime.py
+++ 04.08 Is Prime.py
@@ -1,4 +1,8 @@
 N = int(input("Enter N: "))
 if N>1:
-    for i in range(2,N/2 +1):
-        if i/1
+    for i in range(2,N//2 +1):
+        if N % i == 0:
+            print("COMPOSITE")
+            break
+    else:
+        print("PRIME")

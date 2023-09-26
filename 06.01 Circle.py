02c4d004683642a27b85ec8f77b885760e31f273
clairewebre <142812288+clairewebre@users.noreply.github.com>
2023-09-26 16:29:02 +0000

    content 6

---
+++ 06.01 Circle.py
@@ -0,0 +1,8 @@
+from math import pi
+def diameter(r):
+    return 2*r
+def circumference(r):
+    return 2*pi*r
+def area(r):
+    return pi*(r**2)
+
---
+++ Exam One.py
@@ -0,0 +1,2 @@
+start = int(input("Enter Start of Range: "))
+end = int(input("Enter End of Range: "))

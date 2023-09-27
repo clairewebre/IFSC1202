3047ae8605a6326a29d2d497193ccae59a750ab9
clairewebre <142812288+clairewebre@users.noreply.github.com>
2023-09-27 14:32:42 +0000

    circle

--- 06.01 Circle.py
+++ 06.01 Circle.py
@@ -5,4 +5,12 @@ def circumference(r):
     return 2*pi*r
 def area(r):
     return pi*(r**2)
-
+print("{:>15s}{:>15s}{:>15s}{:>15s}".format("Radius","Diameter","Circumference","Area"))
+radiusfile = "06.01 Radius.txt"
+radius = open(radiusfile, "r")
+r = radius.readline()
+while r !="":
+    fr = float(r)
+    print("{:>15.5f}{:>15.5f}{:>15.5f}{:>15.5f}".format(fr,diameter(fr),circumference(fr),area(fr)))
+    r = radius.readline()
+radius.close()
